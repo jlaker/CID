9ff08dc939d6e3a86206ef25f04dfc746a73af9d
John Laker <jlaker@gmail.com>
2009-02-26 05:26:06 +0000

    changed constraints

--- grails-app/domain/Company.groovy
+++ grails-app/domain/Company.groovy
@@ -1,28 +1,30 @@
 class Company {
-    String name
-    String address1
-    String address2
-    String city
-    String state
-    String zip
-    URL url
-    String phone
-    String fax
-    String parentCompany
-    String noOfEmployees
-    String rssid
-    Date createDate
-    Date updateDate
+  String name
+  String address1
+  String address2
+  String city
+  String state
+  String zip
+  URL url
+  String phone
+  String fax
+  String parentCompany
+  String noOfEmployees
+  String rssid
+  Date createDate
+  Date updateDate
 
-    static constraints = {
-        name(blank: false)
-        address1(blank: false)
-        city(blank: false)
-        state(blank: false)
-        zip(blank: false)
-        url(blank: false, url: true)
-        phone(blank: false)
-        fax(blank: false)
-        rssid(blank: false)
-    }
+  static constraints = {
+    name(blank: false)
+    address1(blank: false)
+    city(blank: false)
+    state(blank: false)
+    zip(blank: false)
+    url(blank: false, url: true)
+    phone(blank: false)
+    fax(blank: false)
+    rssid(blank: false)
+    parentCompany(nullable: true)
+    noOfEmployees(nullable: true)
+  }
 }
