1f2db5ecf1b6a6d1f43f8da3151b8d72c5aaed7d
John Laker <jlaker@gmail.com>
2009-02-27 00:29:52 +0000

    initial checkin

---
+++ src/groovy/EmailSender.groovy
@@ -0,0 +1,73 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: jlaker
+ * Date: Feb 27, 2009
+ * Time: 2:14:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import groovy.text.GStringTemplateEngine
+
+public class EmailSender {
+
+
+  static void main(args) {
+
+    def sql = Sql.newInstance("jdbc:mysql://localhost:3306/cid", "root", "root", "com.mysql.jdbc.Driver")
+    def ds = sql.dataSet('company')
+    def saved
+    def company
+
+    ant = new AntBuilder()
+    ant.mail()
+    def f = new File('email.template')
+    def engine = new GStringTemplateEngine()
+    def binding = ["firstname":"Sam", "lastname":"Pullara"]    
+    template = engine.createTemplate(f).make(binding)
+    println template.toString()
+
+    def fields = new File('/home/jlaker/Documents/CID Surveys/Survey+Email_Addresses+02-11-09.csv').splitEachLine(',') {
+      fields ->
+
+      def rssid = fields[6]
+
+      if (rssid != null && rssid != saved) {
+        saved = fields[6]
+        company = new Company(
+                name: fields[4],
+                rssid: fields[6],
+                address1: " ",
+                address2: " ",
+                city: " ",
+                state: " ",
+                zip: " ",
+                createDate: new Date(),
+                updateDate: new Date(),
+                fax: " "
+        )
+        company.save()
+
+        /*
+        ds.add(
+                name: fields[4],
+                rssid: fields[6],
+                version: 0,
+                address1: " ",
+                address2: " ",
+                city: " ",
+                state: " ",
+                zip: " ",
+                create_date: new Date(),
+                update_date: new Date(),
+                fax: " ",
+                phone: " ",
+                url: " "
+        )
+        */
+      }
+    }
+
+    ds.each { println it.name }
+  }
+
+}
